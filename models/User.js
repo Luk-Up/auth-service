bc8b360f79247711ecb6b3c8cbd249c74e9c278b
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2025-06-08 05:38:38 +0500

    added the db schema

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Provide a username"],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Provide an email"],
+        unique: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            "Enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Provide a password"],
+        minlength: 6,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.getSignedJwtToken = function(){
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN});
+};
+
+module.exports = mongoose.model("User", userSchema);   
